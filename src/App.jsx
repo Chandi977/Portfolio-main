0c7ddfb3ddabbfdb728cbded2c9cad699dbe6c7e
Chandi977 <pappumahato000@gmail.com>
2025-07-02 11:40:21 +0530

    Heartbeat

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
@@ -8,7 +8,25 @@ import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 
+const HEARTBEAT_URL = "https://charan-portfolio-htvx.onrender.com"; // Replace with your backend/server URL
+
 const App = () => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(HEARTBEAT_URL)
+        .then(() => {
+          // Heartbeat sent
+          console.log(`Heartbeat sent: ${res.statusCode}`);
+        })
+        .catch(() => {
+          // Ignore errors
+          console.error(`Heartbeat error: ${e.message}`);
+        });
+    }, 1000 * 60 * 14); // Every 14 minutes
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
